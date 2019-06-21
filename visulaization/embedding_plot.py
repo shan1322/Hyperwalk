b8ebdf48e24c50c91ec1f0866affdc19d7755c52
Shantanu sINGH <shaneyasingh13@gmail.com>
2019-06-21 18:59:22 +0530

    one_hot memory stablelization

--- visulaization/embedding_plot.py
+++ visulaization/embedding_plot.py
@@ -1,23 +1,24 @@
 from sklearn.manifold import TSNE
 import json
 import matplotlib.pyplot as plt
-
+from tqdm import tqdm
 with open("../embeddings/node_embeddings.json") as emb:
     emb = json.load(emb)
+print(emb)
 reduced_embedding = {}
 key, val = [], []
 for i in emb.keys():
     key.append(i)
     val.append(emb[i])
-X_embedded = TSNE(n_components=2).fit_transform(val)
+X_embedded = TSNE(n_components=2,verbose=True).fit_transform(val)
 
 x, y = [], []
-for i in X_embedded:
-    x.append(i[0])
-    y.append(i[1])
+for i in tqdm(range(len(X_embedded))):
+    x.append(X_embedded[i][0])
+    y.append(X_embedded[i][1])
 fig = plt.scatter(x, y)
 
-for i, txt in enumerate(key):
-    plt.annotate(txt, (x[i], y[i]))
+#for i, txt in enumerate(key):
+ #   plt.annotate(txt, (x[i], y[i]))
 
 plt.show()
